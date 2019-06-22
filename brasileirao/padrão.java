d6869f7a83ff953865994c0fcaf413dd0fadb4a9
Pedro Alexandre Pinto <zpasp@live.com>
2019-06-22 16:03:10 -0300

    fotos

---
+++ brasileirao/padrão.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+ 
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+    
+        Scanner in = new Scanner(System.in);
+
+
+ 
+ 
+    }
+ 
+}
